75f4ee7abc944585cd84d5a5f2adb876183a58ce
paopaozhi <paopaozhi@hotmail.com>
2024-08-30 02:08:52 +0800

    feat:use hdm library

--- app/uart-print/src/main.c
+++ app/uart-print/src/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include "cmsis_os2.h"
+#include "hdm.h"
+
+#define UART1 1
 
 void app_main(void)
 {
-    int i = 100;
-    i++;
+    IoTUartInit(UART1, NULL);
+    IoTUartWrite(UART1, "Hello World\n", 12);
     osThreadExit();
 }
